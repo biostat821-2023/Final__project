960db0dd832b34730bc0db95ad6b784d77e866db
Nansu Wang <nw117@duke.edu>
2023-05-02 14:07:41 -0400

    clt_v6

--- src/app.py
+++ src/app.py
@@ -1,4 +1,10 @@
 """Command line tool for database management and data analysis."""
+import sys
+import os
+
+path = os.path.abspath("../")
+sys.path.append(path)
+
 import argparse
 from src.utils.check_db import link_to_database
 from src.main import Patient, Sample
