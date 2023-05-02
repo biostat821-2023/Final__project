d54f16beafd4de396d5c9068a6dda32e9ea7c778
Nansu Wang <nw117@duke.edu>
2023-05-02 14:35:39 -0400

    clt_v11

--- tests/test_clt.py
+++ tests/test_clt.py
@@ -5,18 +5,13 @@ import os
 path = os.path.abspath(".")
 sys.path.append(path)
 
-import subprocess
-from src.app import main
-import sqlite3
-import os
-from unittest.mock import patch
+import subprocess  # noqa: E402
+from src.app import main  # noqa: E402
+import sqlite3  # noqa: E402
 
 
 def test_add_patient(capsys):
-    # args = ["-f", "db", "--db_function", "add", "--db_level patient"]
     inputs = ["12", "Test Name", "Male", "1980-1-1", "123", "test@gmail.com"]
-    # main(args)
-    # patch("builtins.input", side_effect=inputs)
     commands = [
         "python",
         "src/app.py",
@@ -42,5 +37,44 @@ def test_add_patient(capsys):
     assert res[0][4] == 43
     assert res[0][5] == 123
     assert res[0][6] == "test@gmail.com"
+    conn.close()
+    os.remove("database.db")
+
+
+def test_add_sample(capsys):
+    inputs = [
+        "12",
+        "abc",
+        "2019-3-3",
+        "Breast Invasive Carcinoma",
+        9,
+        "No",
+        1.5,
+    ]
+    commands = [
+        "python",
+        "src/app.py",
+        "-f",
+        "db",
+        "--db_function",
+        "add",
+        "--db_level",
+        "sample",
+    ]
+    process = subprocess.Popen(
+        commands, stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True
+    )
+    stdout, _ = process.communicate("\n".join(inputs))
+    conn = sqlite3.connect("database.db")
+    c = conn.cursor()
+    c.execute("SELECT * from Sample where Sample_id = 'abc'")
+    res = c.fetchall()
+    assert res[0][0] == "abc"
+    assert res[0][1] == "12"
+    assert res[0][2] == "2019-03-03 00:00:00"
+    assert res[0][3] == "Breast Invasive Carcinoma"
+    assert res[0][4] == 9
+    assert res[0][5] == "No"
+    assert res[0][6] == 1.5
     conn.close()
     os.remove("database.db")
